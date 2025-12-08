a694c31860d19a6aa7a3bea1b3bf6688270efc4a
Maxim Molokovskikh <mmolokovskikh@gmail.com>
2025-12-08 09:53:13 +0000

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Ingveri Cat" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
